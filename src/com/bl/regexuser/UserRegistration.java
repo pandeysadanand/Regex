1f95ac37c2077adb240914faf627692289e71dad
sadanand pandey <pandeysadanad75@gmail.com>
2022-04-27 22:33:32 +0530

    Added: email validating

--- src/com/bl/regexuser/UserRegistration.java
+++ src/com/bl/regexuser/UserRegistration.java
@@ -4,25 +4,40 @@ import java.util.Scanner;
 import java.util.*;
 
 /**
- * @author sadanand Pandey code for first name validation
+ * @author sadanand Pandey code for last name validation
  *
  */
 public class UserRegistration {
-
 	public static void main(String[] args) {
-		boolean result = firstName("Sadanand");
 
-		if (result) {
-			System.out.println("User registration is valid");
+		boolean result1 = lastName("Pndey");
+		boolean result2 = email("abc.xyz@bl.co.in");
+
+		if (result1) {
+			System.out.print("last name is valid \n");
 		} else {
-			System.out.println("Please enter correct name.");
+			System.out.print("last name is not invalid \n");
 		}
+
+		if (result2) {
+			System.out.print("last name is valid");
+		} else {
+			System.out.print("last name is not invalid");
+		}
+
 	}
 
 	/*
-	 * validating first name and name length must be 3
+	 * last name validating
 	 */
-	public static boolean firstName(String firstName) {
-		return firstName.matches("^[A-Z]{1}[a-z]{2,}");
+	public static boolean lastName(String lastName) {
+		return lastName.matches("^[A-Z]{1}[a-z]{2,}$");
+	}
+
+	/*
+	 * email validating
+	 */
+	public static boolean email(String email) {
+		return email.matches("^[A-Z]{3}[a-zA-z0-9+_.$-]*@[a-z]{2}.[a-z]{2}*[a-z]{2}$");
 	}
 }
