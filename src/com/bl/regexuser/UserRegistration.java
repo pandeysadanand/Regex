a4c06ca74fb6c126ed72e597a1dc0af6061000a6
sadanand pandey <pandeysadanad75@gmail.com>
2022-04-27 22:38:07 +0530

    Added: phone number validating

--- src/com/bl/regexuser/UserRegistration.java
+++ src/com/bl/regexuser/UserRegistration.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.*;
 
 /**
- * @author sadanand Pandey code for last name validation
+ * @author sadanand Pandey
  *
  */
 public class UserRegistration {
@@ -12,6 +12,7 @@ public class UserRegistration {
 
 		boolean result1 = lastName("Pndey");
 		boolean result2 = email("abc.xyz@bl.co.in");
+		boolean result3 = phoneNumber("91 1265487983");
 
 		if (result1) {
 			System.out.print("last name is valid \n");
@@ -23,6 +24,12 @@ public class UserRegistration {
 			System.out.print("last name is valid");
 		} else {
 			System.out.print("last name is not invalid");
+		}
+
+		if (result3) {
+			System.out.print("last name is valid \n");
+		} else {
+			System.out.print("last name is not invalid \n");
 		}
 
 	}
@@ -39,5 +46,12 @@ public class UserRegistration {
 	 */
 	public static boolean email(String email) {
 		return email.matches("^[A-Z]{3}[a-zA-z0-9+_.$-]*@[a-z]{2}.[a-z]{2}*[a-z]{2}$");
+	}
+
+	/*
+	 * phone number validating
+	 */
+	public static boolean phoneNumber(String phoneNumber) {
+		return phoneNumber.matches("^[A-Z]{1}[a-z]{2,}$");
 	}
 }
